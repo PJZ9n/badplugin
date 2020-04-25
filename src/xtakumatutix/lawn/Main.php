6b0691750362475a4788063610dd0fd1a53a28ae
PJZ9n <pjz9n@yahoo.co.jp>
2020-04-26 06:20:23 +0900

    コード修正1

--- src/xtakumatutix/lawn/Main.php
+++ src/xtakumatutix/lawn/Main.php
@@ -10,35 +10,37 @@ use pocketmine\utils\Config;
 
 
 Class Main extends PluginBase implements Listener {
-
+    
+    //$this->なんとかを使うときは、これを書いておかないと面倒なことになる
+    /** @var Config */
+    private $Config;
+    
     public function onEnable(){
         $this->getLogger()->notice("読み込み完了_ver.1.0.0");
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->Config = new Config($this->getDataFolder() . "lawn.yml", Config::YAML);
-	}
-	
-	public function onCommand(CommandSender $sender, Command $command, string $label, array $args):bool
-	{
-		switch($command->getName()){
-			case "lawn":
-		    $this->Config->set($args[0], $args[1]);
-		    $this->Config->save();
-			$sender->sendMessage("{$args[0]}に{$args[1]}のローンを組ませました...あなたは最低です。");
-		}
-		return true;
-
-		case "repayment":
-		    $name = $sender->getPlayer()->getName();
-		        if($this->c->exists($name)){
-			    $lawn = $this->Config->get($name);
-			    $sender->sendMessage("{$args[0]}を返済しました");
-			    $repay = $lawn - $args[0];
-				$this->Config->set($name, $repay);
-		    }else{
-				$sender->sendMessage("あなたは大丈夫よ");
-			}
-		}
-		return true;
-	}
-	return true;
+    }
+    
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args):bool
+    {
+        switch($command->getName()){
+            case "lawn":
+                $this->Config->set($args[0], $args[1]);
+                $this->Config->save();
+                $sender->sendMessage("{$args[0]}に{$args[1]}のローンを組ませました...あなたは最低です。");
+                return true;//ここで処理を終了する
+            case "repayment":
+                $name = $sender->getName();//$senderはそのまま使える(getPlayer関数は存在しない)
+                if($this->Config->exists($name)){
+                    $lawn = $this->Config->get($name);
+                    $sender->sendMessage("{$args[0]}を返済しました");
+                    $repay = $lawn - $args[0];
+                    $this->Config->set($name, $repay);
+                }else{
+                    $sender->sendMessage("あなたは大丈夫よ");
+                }
+                return true;//ここで処理を終了する
+        }
+        return false;//何にも引っかからなかった場合(途中で処理が終了していなかった場合)はfalseを返す
+    }
 }
